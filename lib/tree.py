7c1b4a515d10de70d0d3f988f9de084e84e85d72
brnomosa <omosabria99@gmail.com>
2024-07-01 07:59:36 +0300

    comple

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,19 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    """
+    Returns the node with the given id using depth-first traversal.
+    """
+    def dfs(node):
+      if node is None:
+        return None
+      if node['id'] == id:
+        return node
+      for child in node['children']:
+        result = dfs(child)
+        if result:
+          return result
+      return None
+
+    return dfs(self.root)
+  
